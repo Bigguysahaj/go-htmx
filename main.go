c603221fd430ad42f17994edd02550a40a3ca583
bigguysahaj <sahajssingh2001@gmail.com>
2024-07-03 15:51:11 +0530

    todo-do elements not appending

--- main.go
+++ main.go
@@ -15,7 +15,11 @@ func main() {
 
 	data := map[string][]Todo{
 		"Todos": {
-			Todo{Id: 1, Message: "Buy Milk"},
+			Todo{Id: 1, Message: "Run Go server and HTMX client"},
+			Todo{Id: 2, Message: "Video Editing"},
+			Todo{Id: 3, Message: "Study theo next js"},
+			Todo{Id: 4, Message: "Study theo 3D three js react stuff"},
+			Todo{Id: 5, Message: "Study for iitm exam"},
 		},
 	}
 
@@ -25,8 +29,19 @@ func main() {
 		temp1.Execute(w, data)
 	}
 
+	addTodoHandler := func(w http.ResponseWriter, r *http.Request) {
+		// A variable message, that takes value from the form with label message
+		message := r.PostFormValue("message")
+		// Again parsing html in the new template
+		temp1 := template.Must(template.ParseFiles("index.html"))
+		todo := Todo{Id: len(data["Todos"]) + 1, Message: message}
+		data["Todos"] = append(data["Todos"], todo)
+		temp1.ExecuteTemplate(w, "todo-list-element", todo)
+	}
+
 	// fmt.Println("Helloo")
 	http.HandleFunc("/", todosHandler)
+	http.HandleFunc("/add-todo/", addTodoHandler)
 
 	// log.Fatal here for if something goes wrong, it snaps out console logs the error.
 	log.Fatal(http.ListenAndServe(":8000", nil))
