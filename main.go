f6b9716d8d0e7ddcf4bf5152f9736f621a9c777f
bigguysahaj <sahajssingh2001@gmail.com>
2024-07-03 15:12:37 +0530

    go server and client up and running

--- main.go
+++ main.go
@@ -19,15 +19,15 @@ func main() {
 		},
 	}
 
-	todosHandler := func(w http.ResponseWriter, r http.Request) {
+	todosHandler := func(w http.ResponseWriter, r *http.Request) {
 		temp1 := template.Must(template.ParseFiles("index.html"))
 
-		temp1.Execute(w, nil)
+		temp1.Execute(w, data)
 	}
 
 	// fmt.Println("Helloo")
 	http.HandleFunc("/", todosHandler)
 
 	// log.Fatal here for if something goes wrong, it snaps out console logs the error.
-	log.Fatal(http.ListenAndServer(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
